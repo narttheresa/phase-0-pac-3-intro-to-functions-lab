5b9cc6821770eb44a159bceed70240fe804c49ee
Theresa Tran <theresa1194@gmail.com>
2023-09-12 23:29:48 +1000

    pass test index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+function shout(string) {
+    // todo
+    return string.toUpperCase();
+    "Hello!".toUpperCase();
+
+  }
+
+
+function whisper(string){
+  return string.toLowerCase();
+  "hello!".toLowerCase();
+}
+
+
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+ logShout('spy');
+
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+logWhisper('SPY');
+
+
+
+function sayHiToHeadphonedRoommate(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  } else if ("Let's have dinner together!")
+    return "I would love to!";
+}
+
+
+
+
